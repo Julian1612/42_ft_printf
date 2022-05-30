a2fdde27a4a8366bac8a3c21fb3d2e37c2fbfb28
Julian Schneider <jschneid@2-I-6.42heilbronn.de>
2022-05-30 10:14:21 +0200

    Added %p %x %X

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jschneid <jschneid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 10:56:49 by jschneid          #+#    #+#             */
-/*   Updated: 2022/05/27 15:58:38 by jschneid         ###   ########.fr       */
+/*   Updated: 2022/05/30 10:13:01 by jschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,12 @@ int		output_numbers(va_list arguments);
 int		output_intiger(va_list arguments);
 int		output_percent(void);
 int		output_ptr_adr(va_list arguments);
-int	decimal_to_hexadecimal(unsigned long long decimal_nbr, int counter);
+void	print_ptr_address(unsigned long long decimal_nbr);
+int		get_number_length(long long ptr_address);
+int		output_lowercase_hexa(va_list argument);
+void	print_lowercase_hexa(unsigned long long decimal_nbr);
+int		output_uppercase_hexa(va_list arguments);
+void	print_uppercase_hexa(unsigned long long decimal_nbr);
 
 // 'ft_printf' main function
 int	ft_printf(const char *input_str, ...)
@@ -86,11 +91,11 @@ int	conversion_check(const char *input_str, int index, va_list arguments)
 	else if (input_str[index + 1] == 'i')
 		return (output_numbers(arguments));
 /*	else if (input_str[index + 1] == 'u')
-		return (6);
+		return (6);*/
 	else if (input_str[index + 1] == 'x')
-		return (7);
+		return (output_lowercase_hexa(arguments));
 	else if (input_str[index + 1] == 'X')
-		return (8); */
+		return (output_uppercase_hexa(arguments));
 	else if (input_str[index + 1] == '%')
 		return (output_percent());
 	return (0);
@@ -153,25 +158,40 @@ int	output_percent()
 // CAUTION Function does not return the lenth how many characters the address has !!! needs to be fixed
 int	output_ptr_adr(va_list arguments)
 {
-	int					index;
+	int					length_address;
 	unsigned long long	ptr;
 
-	index = 0;
 	ptr = (unsigned long long) va_arg(arguments, void *);
+	length_address = get_number_length(ptr);
 	write(1, "0x", 2);
-	return (decimal_to_hexadecimal(ptr, index));
+	print_ptr_address(ptr);
+	return (length_address);
+}
+
+int get_number_length(long long ptr_address)
+{
+	int	length;
+
+	length = 0;
+	while (ptr_address > 0)
+	{
+		ptr_address /= 10;
+		if (ptr_address > 0)
+			length ++;
+	}
+	return (length);
 }
 
 // !!!!!!!!!!!!!!!!!!!
 // Function printed zwar die adresse korrekt aber durch die recurssion kann nicht gezahlt werden wie viel characters die addresse hat :( needs to fixed
-int	decimal_to_hexadecimal(unsigned long long decimal_nbr, int counter)
+void	print_ptr_address(unsigned long long decimal_nbr)
 {
 	long long	new_value;
 
 	new_value = decimal_nbr % 16 + 48;
 	decimal_nbr /= 16;
 	if (decimal_nbr > 0)
-		decimal_to_hexadecimal(decimal_nbr, counter);
+		print_ptr_address(decimal_nbr);
 	if (new_value == 58)
 		new_value = 'a';
 	else if (new_value == 59)
@@ -185,7 +205,74 @@ int	decimal_to_hexadecimal(unsigned long long decimal_nbr, int counter)
 	else if (new_value == 63)
 		new_value = 'f';
 	print_char(new_value);
-	return (counter);
+}
+
+int	output_lowercase_hexa(va_list arguments)
+{
+	int					length_address;
+	unsigned long long	ptr;
+
+	ptr = (unsigned long long) va_arg(arguments, void *);
+	length_address = get_number_length(ptr);
+	print_lowercase_hexa(ptr);
+	return (length_address);
+}
+
+void	print_lowercase_hexa(unsigned long long decimal_nbr)
+{
+	long long	new_value;
+
+	new_value = decimal_nbr % 16 + 48;
+	decimal_nbr /= 16;
+	if (decimal_nbr > 0)
+		print_lowercase_hexa(decimal_nbr);
+	if (new_value == 58)
+		new_value = 'a';
+	else if (new_value == 59)
+		new_value = 'b';
+	else if (new_value == 60)
+		new_value = 'c';
+	else if (new_value == 61)
+		new_value = 'd';
+	else if (new_value == 62)
+		new_value = 'e';
+	else if (new_value == 63)
+		new_value = 'f';
+	print_char(new_value);
+}
+
+int	output_uppercase_hexa(va_list arguments)
+{
+	int					length_address;
+	unsigned long long	ptr;
+
+	ptr = (unsigned long long) va_arg(arguments, void *);
+	length_address = get_number_length(ptr);
+	print_uppercase_hexa(ptr);
+	return (length_address);
+}
+
+void	print_uppercase_hexa(unsigned long long decimal_nbr)
+{
+	long long	new_value;
+
+	new_value = decimal_nbr % 16 + 48;
+	decimal_nbr /= 16;
+	if (decimal_nbr > 0)
+		print_uppercase_hexa(decimal_nbr);
+	if (new_value == 58)
+		new_value = 'A';
+	else if (new_value == 59)
+		new_value = 'B';
+	else if (new_value == 60)
+		new_value = 'C';
+	else if (new_value == 61)
+		new_value = 'D';
+	else if (new_value == 62)
+		new_value = 'E';
+	else if (new_value == 63)
+		new_value = 'F';
+	print_char(new_value);
 }
 
 int	main ()
@@ -196,12 +283,12 @@ int	main ()
 
 	printf("////////////////Original////////////////\n");
 	printf("output: ");
-	y = printf("%p", ptr);
+	y = printf("%X %x", 1299993, 4444444);
 	printf("\nchars: %i", y);
 	printf("\n///////////////////My///////////////////\n");
 	printf("output: ");
 	fflush(stdout);
-	x = ft_printf("%p", ptr);
+	x = ft_printf("%X %x", 1299993, 4444444);
 	printf("\nchars: %i", x);
 	return (0);
 }
